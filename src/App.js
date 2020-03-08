e2741f12e2d7af555767bdd6a6b8ea005c1cb7d5
Diego Arevalo <diego.arevalo.97@hotmail.com>
2020-03-08 22:55:26 +0000

    Creando Proyecto

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React,{ Component } from 'react';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = { displayBio: false};
+
+        this.toogleDisplayBio = this.toogleDisplayBio.bind(this);
+    }
+    
+    //Metodo que oculta y muestra la biografia
+    toogleDisplayBio(){
+        this.setState({displayBio: !this.state.displayBio })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Diego Arevalo, I am Computer Enginner</p>
+                <p>I am always learn something different</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                        <p> I live in Dublin, and I love Practice Table tennis </p>
+                        <p>Now I start to learn React.js</p>
+                        <button onClick = {this.toogleDisplayBio}>Show Less</button>
+                        </div>
+                    ) : (
+                       <div>
+                           <button onClick = {this.toogleDisplayBio}>Read More</button>
+                       </div> 
+                    )
+                }
+            </div>
+        )
+    }
+}
+
+export default App;
