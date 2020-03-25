d8c0f297dd89bd77a6f8cab72dc2defb0767873f
Diego Arevalo <diego.arevalo.97@hotmail.com>
2020-03-25 16:50:12 +0000

    Updates

---
+++ precache-manifest.50b6598a8cee5f1a025f2e72333735c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112d6da337de5bcf77783b498e0d564e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "53db1adfeaebe29c6c47",
+    "url": "/Portfolio/static/css/main.8b548ac0.chunk.css"
+  },
+  {
+    "revision": "7e3a487a835cafb16b82",
+    "url": "/Portfolio/static/js/2.0e1f0f21.chunk.js"
+  },
+  {
+    "revision": "53db1adfeaebe29c6c47",
+    "url": "/Portfolio/static/js/main.7d32b88a.chunk.js"
+  },
+  {
+    "revision": "a3a852c4426cab9149aa",
+    "url": "/Portfolio/static/js/runtime~main.4b3de30f.js"
+  },
+  {
+    "revision": "7cef10cf0f16dba6be02449467f47b09",
+    "url": "/Portfolio/static/media/Code.7cef10cf.jpg"
+  },
+  {
+    "revision": "8dae4a783c5e8fd248081bed8436bf7c",
+    "url": "/Portfolio/static/media/Diego.8dae4a78.jpg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/Portfolio/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "9fd9e7f873593696b4e974abc042200d",
+    "url": "/Portfolio/static/media/project1.9fd9e7f8.jpg"
+  },
+  {
+    "revision": "d99ed06ecfce92f8a30d0ad7d15de6f8",
+    "url": "/Portfolio/static/media/project2.d99ed06e.jpg"
+  },
+  {
+    "revision": "c7ee309625b0e3105cb6c6577f115475",
+    "url": "/Portfolio/static/media/project3.c7ee3096.jpg"
+  }
+]);
